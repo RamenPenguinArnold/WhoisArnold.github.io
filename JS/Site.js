aae29be81f965ec60b343de565c12383fa63a7e2
Arnold <mekonnen@pdx.edu>
2024-12-11 15:48:29 -0800

    Made name and email required

--- JS/Site.js
+++ JS/Site.js
@@ -1,7 +1,8 @@
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+  
   console.log("Name:", form.elements[1].value);
   console.log("Email:", form.elements[2].value);
+  event.preventDefault();
 });
