2b2aaceaf7bf41a1c76e0e29d503f7de05385b9f
Arnold <mekonnen@pdx.edu>
2024-12-11 16:26:42 -0800

    Small js change with local storage

--- JS/Site.js
+++ JS/Site.js
@@ -4,5 +4,11 @@ form.addEventListener("submit", (event) => {
   
   console.log("Name:", form.elements[1].value);
   console.log("Email:", form.elements[2].value);
+  var inputName = form.elements[1].value;
+  var inputEmail = form.elements[2].value;
+  localStorage.setItem("Name",inputName);
+  localStorage.setItem("Email",inputEmail);
+  const check = document.getElementById("checker");
+  check.textContent = "Is this right," +  localStorage.getItem("Name")+"?" +"\n"+ "The Email is " +localStorage.getItem("Email");
   event.preventDefault();
 });
